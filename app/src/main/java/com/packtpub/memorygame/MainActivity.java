2ed6c8177c1ac5176cae2f910b554fd56bbacb7b
Teoh Kheng Hong <tkhenghong@gmail.com>
2018-12-04 07:59:41 +0800

    UI design

--- app/src/main/java/com/packtpub/memorygame/MainActivity.java
+++ app/src/main/java/com/packtpub/memorygame/MainActivity.java
@@ -1,13 +1,29 @@
 package com.packtpub.memorygame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Make a button from the button in our layout
+        Button button =(Button) findViewById(R.id.button);
+
+        //Make each it listen for clicks
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
     }
 }
