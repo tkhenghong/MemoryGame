f2bd347c3bd0f124b10c13d46f962f8111757daf
Teoh Kheng Hong <tkhenghong@gmail.com>
2018-12-08 23:22:18 +0800

    Memory game memory logic, sound and Animations until page 202

--- app/src/main/java/com/packtpub/memorygame/GameActivity.java
+++ app/src/main/java/com/packtpub/memorygame/GameActivity.java
@@ -1,5 +1,6 @@
 package com.packtpub.memorygame;
 
+import android.content.SharedPreferences;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.media.AudioManager;
@@ -9,11 +10,16 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Random;
+
+import static com.packtpub.memorygame.MainActivity.hiScore;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -47,10 +53,27 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     int playerScore;
     boolean isResponding;
 
+    //for our hiscore (phase 4)
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    String dataName = "MyData";
+    String intName = "MyInt";
+    int defaultInt = 0;
+    int hiScore;
+
+    Animation wobble;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        wobble = AnimationUtils.loadAnimation(this, R.anim.wobble); // Create a folder called anim in res folder, then create wobble
+
+        //phase 4
+        //initialize our two SharedPreferences objects
+        prefs = getSharedPreferences(dataName,MODE_PRIVATE);
+        editor = prefs.edit();
+        hiScore = prefs.getInt(intName, defaultInt);
 
         // Get sound files ready to be played
         soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
@@ -91,6 +114,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         button4.setOnClickListener(this);
         buttonReplay.setOnClickListener(this);
 
+
+
         //This is the code which will define our thread
         myHandler = new Handler() {
             public void handleMessage(Message msg) {
@@ -98,32 +123,36 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 if (playSequence) {
                     //All the thread action will go here
                     //make sure all the buttons are made visible
-                    button1.setVisibility(View.VISIBLE);
-                    button2.setVisibility(View.VISIBLE);
-                    button3.setVisibility(View.VISIBLE);
-                    button4.setVisibility(View.VISIBLE);
+//                    button1.setVisibility(View.VISIBLE);
+//                    button2.setVisibility(View.VISIBLE);
+//                    button3.setVisibility(View.VISIBLE);
+//                    button4.setVisibility(View.VISIBLE);
                     switch (sequenceToCopy[elementToPlay]) {
                         case 1:
-                            //hide a button
-                            button1.setVisibility(View.INVISIBLE);
+                            //hide a button - NOT ANYMORE
+                            // button1.setVisibility(View.INVISIBLE);
+                            button1.startAnimation(wobble); //this will make button1 wobble
                             //play a sound
                             soundPool.play(sample1, 1, 1, 0, 0, 1);
                             break;
                         case 2:
-                            //hide a button
-                            button2.setVisibility(View.INVISIBLE);
+                            //hide a button - NOT ANYMORE
+                            // button2.setVisibility(View.INVISIBLE);
+                            button2.startAnimation(wobble); //this will make button2 wobble
                             //play a sound
                             soundPool.play(sample2, 1, 1, 0, 0, 1);
                             break;
                         case 3:
-                            //hide a button
-                            button3.setVisibility(View.INVISIBLE);
+                            //hide a button - NOT ANYMORE
+                            // button3.setVisibility(View.INVISIBLE);
+                            button3.startAnimation(wobble); //this will make button3 wobble
                             //play a sound
                             soundPool.play(sample3, 1, 1, 0, 0, 1);
                             break;
                         case 4:
                             //hide a button
-                            button4.setVisibility(View.INVISIBLE);
+                            // button4.setVisibility(View.INVISIBLE);
+                            button4.startAnimation(wobble); //this will make button4 wobble
                             //play a sound
                             soundPool.play(sample4, 1, 1, 0, 0, 1);
                             break;
@@ -164,25 +193,79 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     public void sequenceFinished() {
         playSequence = false;
         //make sure all the buttons are made visible
-        button1.setVisibility(View.VISIBLE);
-        button2.setVisibility(View.VISIBLE);
-        button3.setVisibility(View.VISIBLE);
-        button4.setVisibility(View.VISIBLE);
+//        button1.setVisibility(View.VISIBLE);
+//        button2.setVisibility(View.VISIBLE);
+//        button3.setVisibility(View.VISIBLE);
+//        button4.setVisibility(View.VISIBLE);
         textWatchGo.setText("GO!");
         isResponding = true;
     }
 
     @Override
     public void onClick(View v) {
-        if(!playSequence) {
+        if (!playSequence) {
             switch (v.getId()) {
                 //case statements here....
-                case R.id.button:
+                case R.id.button1:
                     // play a sound
-                    soundPool.play(sample1,1,1,0,0,1)
+                    soundPool.play(sample1, 1, 1, 0, 0, 1);
+                    checkElement(1);
+                    break;
+                case R.id.button2:
+                    //play a sound
+                    soundPool.play(sample2, 1, 1, 0, 0, 1);
+                    checkElement(2);
+                    break;
+                case R.id.button3:
+                    //play a sound
+                    soundPool.play(sample3, 1, 1, 0, 0, 1);
+                    checkElement(3);
+                    break;
+                case R.id.button4:
+                    //play a sound
+                    soundPool.play(sample4, 1, 1, 0, 0, 1);
+                    checkElement(4);
+                    break;
+                case R.id.buttonReplay:
+                    difficultyLevel = 3;
+                    playerScore = 0;
+                    textScore.setText("Score: " + playerScore);
+                    playASequence();
                     break;
             }
         }
 
+    }
+
+    public void checkElement(int thisElement) {
+        if (isResponding) {
+            playerResponses++;
+            if (sequenceToCopy[playerResponses - 1] == thisElement) { // Correct
+                playerScore = playerScore + ((thisElement + 1) * 2);
+                textScore.setText("Score: " + playerScore);
+                if (playerResponses == difficultyLevel) {//got the whole sequence
+                    //don't checkElement anymore
+                    isResponding = false;
+
+                    //now raise the difficulty
+                    difficultyLevel++;
+                    //and play another sequence
+                    playASequence();
+                }
+            } else { //wrong answer
+                textWatchGo.setText("FAILED!");
+                //don't checkElement anymore
+                isResponding = false;
+
+                //for our high score (phase 4)
+                if(playerScore > hiScore) {
+                    hiScore = playerScore;
+                    editor.putInt(intName, hiScore);
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "New Hiscore",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/packtpub/memorygame/MainActivity.java
+++ app/src/main/java/com/packtpub/memorygame/MainActivity.java
@@ -1,17 +1,38 @@
 package com.packtpub.memorygame;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //for our hiscore (phase 4)
+    SharedPreferences prefs;
+    String dataName = "MyData";
+    String intName = "MyInt";
+    int defaultInt = 0;
+    //both activities can see this
+    public static int hiScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //for our high score (phase 4)
+        //initialize our two SharedPreferences objects
+        prefs = getSharedPreferences(dataName,MODE_PRIVATE);
+        //Either load our High score or
+        //if not available our default of 0
+        hiScore = prefs.getInt(intName, defaultInt);
+        //Make a reference to the Hiscore textview in our layout
+        TextView textHiScore =(TextView) findViewById(R.id.textHiScore);
+        //Display the hi score
+        textHiScore.setText("Hi: "+ hiScore);
 
         //Make a button from the button in our layout
         Button button =(Button) findViewById(R.id.button);
